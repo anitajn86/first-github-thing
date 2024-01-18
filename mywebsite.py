3622bd705dd7ebda34090f1a764fbaa911892b54
anitajn86 <anitajn7990@gmail.com>
2024-01-18 09:07:15 +0300

    added features

--- mywebsite.py
+++ mywebsite.py
@@ -1,6 +1,7 @@
-maximumNumber=[25,70,90,25]
+maximumNumber=[25,70,85,25]
+girls=["Joy","liz","Anita"]
 maximum=0
-for n in maximumNumber:
-    if(maximum<n):
-        maximum=n
+for num in maximumNumber:
+    if(maximum<num):
+        maximum=num
 print(maximum)
