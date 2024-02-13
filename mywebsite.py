dd9ba14d9380b0bfbb6226a33a4e57d7d0effb4a
Anita <anitajn7990@gmail.com>
2024-02-13 23:46:08 +0300

    data structures including fibonacci numbers

--- mywebsite.py
+++ mywebsite.py
@@ -5,3 +5,25 @@ for num in maximumNumber:
     if(maximum<num):
         maximum=num
 print(maximum)
+
+prev2=0
+prev1=1
+print(prev2,prev1)
+for fibo in range(20):
+    newFibo=prev2+prev1
+    print(newFibo)
+    prev2=prev1
+    prev1=newFibo
+
+def fibonacci(prev1,prev2):
+    global count
+    if count<=20:
+        newFibo=prev1+prev2
+        print(newFibo)
+        prev2=prev1
+        prev1=newFibo
+        count+=1
+        fibonacci(prev1,prev2)
+    else:
+        return
+fibonacci(1,0)
